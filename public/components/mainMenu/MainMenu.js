b68e392170e64d3ad62bb64084c99349c65b5ce3
JaRoD41 <laurent.trinco@gmail.com>
2024-05-02 15:07:47 +0200

    mise en place base boutons

--- public/components/mainMenu/MainMenu.js
+++ public/components/mainMenu/MainMenu.js
@@ -1,13 +1,53 @@
 class MainMenu {
   constructor() {
     this.utils = new Utils()
+    this.root = document.querySelector('#root')
+    this.pageName = null
   }
 
   // TODO : A TOI DE JOUER LOLO 🤣 Exemple classe dans header/Header.js | MainMenu.js = index.html (entry point, menu général)
   // http://192.168.0.254:8080/usv_prod/litigesApp/public/views/affectation.html (1ère URL, (A affecter)
 
+  async render() {
+    const header = document.createElement('header')
 
-  async initMainMenu() {}
+    const link = document.createElement('a')
+    link.href = 'http://192.168.0.254:8080/usv_prod/menugeneral.asp'
+    link.title = 'Menu général'
+
+    // Création du conteneur du menu
+    const menuContainer = document.createElement('div')
+    menuContainer.classList.add('menu-container')
+
+    // Création de la barre de boutons horizontaux
+    const horizontalBar = document.createElement('div')
+    horizontalBar.classList.add('horizontal-bar')
+    for (let i = 0; i < 4; i++) {
+      const button = document.createElement('button')
+      button.textContent = `Bouton H${i + 1}`
+      horizontalBar.appendChild(button)
+    }
+
+    // Création de la barre de boutons verticaux
+    const verticalBar = document.createElement('div')
+    verticalBar.classList.add('vertical-bar')
+    for (let j = 0; j < 3; j++) {
+      const button = document.createElement('button')
+      button.textContent = `Bouton V${j + 1}`
+      verticalBar.appendChild(button)
+    }
+
+    // Ajout des barres au conteneur du menu
+    menuContainer.appendChild(horizontalBar)
+    menuContainer.appendChild(verticalBar)
+
+    // Ajout du menu au root de la page
+    this.root.appendChild(menuContainer)
+  }
+
+  async initMainMenu() {
+    await this.render()
+  }
 }
 
 const mainMenu = new MainMenu()
