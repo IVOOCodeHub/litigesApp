0b2f914348d95f54734243e7f90d5152abe3ef4a
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2024-05-13 10:35:33 +0200

    initValidation table

--- public/controller/validation/Validation.js
+++ public/controller/validation/Validation.js
@@ -12,10 +12,90 @@ class Validation {
     this.root.appendChild(this.main)
   }
 
+  async initForm() {
+    const section = document.createElement('section')
+    section.setAttribute('id', 'searchFolder')
+
+    section.innerHTML = `
+    <div class="inputWrapper">
+        <label>Destinataire: </label>
+        <select>
+          <option>Toutes</option>
+          <option>mockup1</option>
+          <option>mockup2</option>
+          <option>mockup3</option>
+          <option>mockup4</option>
+          <option>mockup5</option>
+        </select>
+    </div>
+    <div class="inputWrapper">
+        <label>Émetteur: </label>
+        <select>
+          <option>Toutes</option>
+          <option>mockup1</option>
+          <option>mockup2</option>
+          <option>mockup3</option>
+          <option>mockup4</option>
+          <option>mockup5</option>
+        </select>
+    </div>
+    
+    <div class="inputWrapper">
+        <button>Gérer les sortants</button>
+        <button>Gérer les internes</button>
+    </div>
+    
+    `
+    this.main.appendChild(section)
+  }
+
+  async initTable() {
+    this.section = document.createElement('section')
+    this.section.setAttribute('id', 'tableContainer')
+    this.section.innerHTML += `
+        <table>
+          <thead>
+              <tr>
+                <th>Réf.</th>
+                <th>Destinataire</th>
+                <th>Émetteur</th>
+                <th>Date</th>
+                <th>Pièce</th>
+                <th>Réf.doc.</th>
+                <th>Sélectionner</th>
+              </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td>mockupRef</td>
+                <td>mockupDesti</td>
+                <td>mockupEmet</td>
+                <td>mockupDate</td>
+                <td>mockupPiece</td>
+                <td>monckupRefDoc</td>
+                <td><input type="checkbox"></td>
+            </tr>
+            <tr>
+                <td>mockupRef2</td>
+                <td>mockupDesti2</td>
+                <td>mockupEmet2</td>
+                <td>mockupDate2</td>
+                <td>mockupPiece2</td>
+                <td>monckupRefDoc2</td>
+                <td><input type="checkbox"></td>
+            </tr>
+          </tbody>
+        </table>
+    `
+    this.main.appendChild(this.section)
+  }
+
   async initEventListeners() {}
 
   async initValidation() {
     await this.initMain()
+    await this.initForm()
+    await this.initTable()
     await this.footer.initFooter()
     await this.initEventListeners()
   }
