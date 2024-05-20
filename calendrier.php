a249901cff7b9d34c0a7cd4e7259bb0b8590662b
JaRoD41 <laurent.trinco@gmail.com>
2024-05-20 14:02:40 +0200

    calendrier.php mis a jour avec requete OK

--- calendrier.php
+++ calendrier.php
@@ -17,39 +17,32 @@ try {
   $conn = new PDO("sqlsrv:server=$serverName;Database=$databaseName", $userName, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // Première requête
-  $sql1 = "SELECT Count(cle) AS n, societe FROM IVOO.dbo.courrier
-            WHERE service='FINANCE' AND statut='DISTRIBUE' AND action='A TRAITER'
-            AND cle_prevision=0 AND cle_paiement=0 AND date_piece > '31/12/2017 23:59:59'
-            AND nature <> 'FACTUREC' AND nature <> 'CHEQUE' AND nature <> 'ACCORD PART'
-            GROUP BY societe ORDER BY societe;";
-  $stmt1 = $conn->prepare($sql1);
-  $stmt1->execute();
-  $results1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+  // Requête SQL unique
+  $sql = "SELECT cle,
+                societe,
+                tiers,
+                nom,
+                commentaire,
+                conseil,
+                theme,
+                multiple,
+                statut,
+                datedebut,
+                datefin,
+                datepaction,
+                paction
+          FROM IVOO.dbo.litiges_dossier
+          ORDER BY datedebut;";
 
-  // Deuxième requête
-  $sql2 = "SELECT cle, societe, action, dh_saisie, auteur_saisie, service, action, commentaire, statut, nature, societe_emettrice FROM IVOO.dbo.courrier
-            WHERE service='FINANCE' AND statut='DISTRIBUE' AND action='A TRAITER'
-            AND cle_prevision=0 AND date_piece > '31/12/2017 23:59:59'
-            AND nature <> 'FACTUREC' AND nature <> 'CHEQUE' AND nature <> 'ACCORD PART'
-            ORDER BY cle;";
-  $stmt2 = $conn->prepare($sql2);
-  $stmt2->execute();
-  $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  // Troisième requête
-  $sql3 = "SELECT nature_piece FROM IVOO.dbo.courrier_profils_distribution
-            GROUP BY nature_piece ORDER BY nature_piece;";
-  $stmt3 = $conn->prepare($sql3);
-  $stmt3->execute();
-  $results3 = $stmt3->fetchAll(PDO::FETCH_ASSOC);
-
-  // Combinez tous les résultats en un seul objet JSON
-  $finalResults = ['societes' => $results1, 'courriers' => $results2, 'nature_pieces' => $results3];
-  echo json_encode($finalResults);
+  // Renvoie des résultats sous forme de JSON
+  echo json_encode($results);
 
 } catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  http_response_code(500);
+  echo json_encode(["error" => "Connection failed: " . $e->getMessage()]);
 }
-
 ?>
