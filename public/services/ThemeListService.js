ba55b3b6d26267e3467f7ce8cd9d36abef9b6c19
JaRoD41 <laurent.trinco@gmail.com>
2024-06-26 11:50:44 +0200

    del comments

--- public/services/ThemeListService.js
+++ public/services/ThemeListService.js
@@ -16,37 +16,6 @@
 //     return response.json()
 //   }
 
-//   async addTheme(theme) {
-//     // Make an API call to add a new theme
-//     const response = await fetch('/api/themes/add', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(theme),
-//     })
-
-//     if (!response.ok) {
-//       throw new Error('Failed to add theme')
-//     }
-//   }
-
-//   async updateTheme(theme) {
-//     // Make an API call to update an existing theme
-//     const response = await fetch(`/api/themes/update/${theme.id}`, {
-//       method: 'PUT',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(theme),
-//     })
-
-//     if (!response.ok) {
-//       throw new Error('Failed to update theme')
-//     }
-//   }
-// }
-
 class ThemeListService extends ApiCalls {
   constructor() {
     super()
